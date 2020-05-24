0269487e189504379b5062dd7dc5c7903b5be8d7
v821870 <satendra.singh@united.com>
2020-05-25 02:58:32 +0530

    Successfully implemented security

--- books-client/src/main/java/org/pratap/sb/ex/booksclient/booksclient/BooksClientApplication.java
+++ books-client/src/main/java/org/pratap/sb/ex/booksclient/booksclient/BooksClientApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
+@EnableResourceServer
 @SpringBootApplication
+@ComponentScan({"org.pratap.sb.ex.booksclient.booksclient"})
 public class BooksClientApplication {
 
 	public static void main(String[] args) {
